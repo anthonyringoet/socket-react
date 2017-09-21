ab892b48bcb203f8951969917c5a2ba4486b0765
Anthony Ringoet <anthonyringoet@gmail.com>
2017-09-21 22:19:41 +0200

    Add prettier single quote

--- server/app.js
+++ server/app.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
-const port = process.env.PORT || 4001
-const index = require('./routes/index')
-const app = express()
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const port = process.env.PORT || 4001;
+const index = require('./routes/index');
+const app = express();
 
-app.use(index)
+app.use(index);
 
-const server = http.createServer(app)
-const io = socketIo(server)
+const server = http.createServer(app);
+const io = socketIo(server);
 const getDataAndEmit = async socket => {
   try {
     socket.emit('info', {
       uptime: process.uptime(),
       cpu: process.cpuUsage()
-    })
+    });
   } catch (error) {
-    console.error({error})
+    console.error({ error });
   }
-}
+};
 
-let interval
+let interval;
 io.on('connection', socket => {
-  console.log('new client connected')
+  console.log('new client connected');
   if (interval) {
-    clearInterval(interval)
+    clearInterval(interval);
   }
-  interval = setInterval(() => getDataAndEmit(socket), 50)
+  interval = setInterval(() => getDataAndEmit(socket), 50);
 
   socket.on('disconnect', () => {
-    console.log('client disconnected')
-  })
-})
+    console.log('client disconnected');
+  });
+});
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`));
